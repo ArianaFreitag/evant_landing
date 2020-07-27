6cb3af59ae85ac82474fe019ab6ac5f63593014a
Ariana Freitag <freitagariana@gmail.com>
2020-07-27 18:36:04 -0400

    in progress

--- action.js
+++ action.js
@@ -1,56 +1,56 @@
 function startup() {
-  var video = document.getElementById("watchMe");
+  var video2 = document.getElementById("watchMe");
   var overlay = document.getElementById("overlay");
-  var video_icon = document.getElementById("virt_button");
+  var video2_button = document.getElementById("virt_button");
   var video1 = document.getElementById("video1");
   var video1_button = document.getElementById("play_img");
   var bg = document.getElementById("video_background");
   var bg2 = document.getElementById("video_background_2");
 
-  video.addEventListener(
+  video2.addEventListener(
     "ended",
     function() {
       console.log("ended");
-      video.style.display = "none";
+      video2.style.display = "none";
     },
     false
   );
 
-  video_icon.addEventListener("click", function() {
+  video1.addEventListener(
+    "ended",
+    function() {
+      console.log("ended");
+      video1.style.display = "none";
+    },
+    false
+  );
+
+  video1_button.addEventListener("click", function() {
     bg.style.display = "block";
-    video.style.display = "block";
-    video.play();
+    video1.style.display = "block";
+    video1.play();
     $("body").css("overflow-y", "hidden");
   });
 
-  // video.addEventListener("click", function() {
-  //   video.pause();
-  //   video.currentTime = 0;
-  //   bg.style.display = "none";
-  //   video.style.display = "none";
-  //   $("body").css("overflow-y", "scroll");
-  // });
+  video2_button.addEventListener("click", function() {
+    bg.style.display = "block";
+    video2.style.display = "block";
+    video2.play();
+    $("body").css("overflow-y", "hidden");
+  });
 
   bg.addEventListener("click", function() {
     video1.pause();
-    video1.currentTime = 0;
     bg.style.display = "none";
     video1.style.display = "none";
     $("body").css("overflow-y", "scroll");
   });
 
   bg2.addEventListener("click", function() {
-    video.pause();
-    video.currentTime = 0;
+    video2.pause();
+    video2.style.display = "none";
     bg2.style.display = "none";
-    video.style.display = "none";
     $("body").css("overflow-y", "scroll");
-  });
-
-  video1_button.addEventListener("click", function() {
-    bg.style.display = "block";
-    video1.style.display = "block";
-    $("body").css("overflow-y", "hidden");
   });
 }
 
