c44b3b35214afce90d960b31a3625cbda815e9fc
Ariana Freitag <freitagariana@gmail.com>
2020-07-30 23:49:29 -0400

    added call

--- action.js
+++ action.js
@@ -1,3 +1,6 @@
+var form = document.getElementById("email_form");
+var email = document.getElementById("email_input");
+
 function startup() {
   var video2 = document.getElementById("video2");
   var video2_button = document.getElementById("virt_button");
@@ -5,6 +8,9 @@ function startup() {
   var video1_button = document.getElementById("play_img");
   var bg = document.getElementById("video_background");
   var bg2 = document.getElementById("video_background_2");
+  var signUpButton = document.getElementById("req_access");
+  var form = document.getElementById("email_form");
+  var email = document.getElementById("email_input");
 
   video2.addEventListener(
     "ended",
@@ -53,35 +59,41 @@ function startup() {
     bg2.style.display = "none";
     $("body").css("overflow-y", "scroll");
   });
+
+  var httpRequest = new XMLHttpRequest();
+
+  httpRequest.addEventListener("readystatechange", function() {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+
+  form.addEventListener("submit", function() {
+    data = JSON.stringify({ email: email.value });
+
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === 4) {
+        callback(xhr.responseText);
+      }
+    };
+
+    if (ValidateEmail(email.value)) {
+      httpRequest.open("post", "https://dev.evant.app/requestAccess");
+      httpRequest.setRequestHeader("Content-Type", "application/json");
+      httpRequest.send(data);
+    } else {
+      console.log("invalid email");
+    }
+  });
+
+  // form.dispatchEvent(new Event("submit"));
 }
 
-$("#email_input").submit(function(e) {
-  console.log("here");
-  e.preventDefault(); // avoid to execute the actual submit of the form.
-
-  var form = $(this);
-  var url = form.attr("action");
-  if (ValidateEmail(form.serialize())) {
-    $.ajax({
-      type: form.attr("method"),
-      url: url,
-      data: form.serialize(), // serializes the form's elements.
-      success: function(data) {
-        alert(data);
-      }
-    });
-  } else {
-    console.log("invalid email");
-  }
-});
-
 function ValidateEmail(mail) {
-  if (
-    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)
-  ) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
     return true;
   }
-  alert("You have entered an invalid email address!");
+  // alert("You have entered an invalid email address!");
   return false;
 }
 
