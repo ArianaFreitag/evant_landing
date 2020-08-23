6fee43aed0ec2aa331a4113483df1683042de5c9
Ariana Freitag <freitagariana@gmail.com>
2020-08-23 14:14:46 -0400

    solutions icons

--- hover.js
+++ hover.js
@@ -1,14 +1,23 @@
-$(document).ready(function() {
-  $("networking_text").hover(
-    function() {
-      image = document.getElementById("networking_box");
-      image.src = "atributes/networking_yellow.png";
-      console.log("on");
-    },
-    function() {
-      image = document.getElementById("networking_box");
-      image.src = "atributes/networking_purple.png";
-      console.log("off");
-    }
-  );
-});
+// $(document).ready(function() {
+//   $("networking_text").hover(
+//     function() {
+//       image = document.getElementById("networking_text");
+//       image.src = "atributes/networking_yellow.png";
+//       console.log("on");
+//     },
+//     function() {
+//       image = document.getElementById("networking_text");
+//       image.src = "atributes/networking_purple.png";
+//       console.log("off");
+//     }
+//   );
+// });
+
+// element = document.getElementById("networking_box");
+// function hover(element) {
+//   element.setAttribute("src", "atributes/networking_yellow.png");
+// }
+//
+// function unhover(element) {
+//   element.setAttribute("src", "atributes/networking_purple.png");
+// }
